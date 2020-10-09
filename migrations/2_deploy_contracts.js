95e981d8f6236d6712f7e0471ab3bd66451d8c4c
Ricardo Bustos <eros@polispay.org>
2020-10-09 12:26:22 -0500

    Change Migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,51 @@
 const Factory = artifacts.require('uniswapv2/uniswap-factory/UniswapV2Factory.sol');
-const Sushi = artifacts.require('token/SushiToken.sol');
+const Bamboo = artifacts.require('token/BambooToken.sol');
 const WETH = artifacts.require('weth/WETH.sol');
 const Router = artifacts.require('uniswapv2/uniswap-router/UniswapV2Router02.sol')
 
 
 module.exports = async function(deployer, _network, addresses) {
+  
+  if(_network === 'ropsten') {
+    /* Deploy BambooToken if not on testnet
+      await deployer.deploy(Bamboo);
+      const bamboo = await Bamboo.deployed()
+      bamboo.mint(addresses[0], web3.utils.toWei('8666666'))
+    */
+
+    // Get WETH contact in ropsten
+    weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
+
+    // Deploy Factory
+    await deployer.deploy(Factory, addresses[0]);
+    const factory = await Factory.deployed();
+    
+    // Deploy Router
+    await deployer.deploy(Router, factory.address, WETH.address)
+    const router = await Router.deployed();
+
+  } else {
+
+    // Deploy WETH contract 
+    await deployer.deploy(WETH);
+    const weth = await WETH.deployed();
+  
+    // Deploy bamboo and mint the pre-mine
+    await deployer.deploy(Bamboo);
+    const bamboo = await Bamboo.deployed()
+    bamboo.mint(addresses[0], web3.utils.toWei('8666666'))
+  
+    // Deploy factory
+    await deployer.deploy(Factory, addresses[0]);
+    const factory = await Factory.deployed();
+  
+    // Deploy Router
+    await deployer.deploy(Router, factory.address, WETH.address)
+    const router = await Router.deployed();
+  }
 
 
+  /*
   await deployer.deploy(WETH);
   const weth = await WETH.deployed();
 
@@ -22,12 +61,6 @@ module.exports = async function(deployer, _network, addresses) {
   await deployer.deploy(Router, factory.address, WETH.address)
   const router = await Router.deployed();
   
-  /*
-
-  */
-
-
-  /*
 
 
   
@@ -38,8 +71,9 @@ module.exports = async function(deployer, _network, addresses) {
   await factory.createPair(weth.address, tokenA.address);
   await factory.createPair(weth.address, tokenB.address);
   await deployer.deploy(Router, factory.address, "0x74Cd334a4A6A121d47481689F2f8350156c111FF");
-
   */
+
+
   /*
   const router = await Router.deployed();
 
