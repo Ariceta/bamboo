d3da49c306e6efbe09f2d383925a664db89ce3d1
Aether-eum <aether@polispay.org>
2020-12-16 11:38:17 -0600

    truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -4,33 +4,39 @@ const web3 = new Web3();
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
-    networks: {
-        development: {
-            host: "127.0.0.1",
-            port: 8545,
-            network_id: "*"
-        },
-        ropsten: {
-            provider: function() {
-                return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`)
-            },
-            network_id: 3,
-            gas: 7000000      //make sure this gas allocation isn't over 4M, which is the max
-        },
-        kovan: {
-            provider: function() {
-                return new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${projectId}`)
-            },
-            network_id: 42,
-            gas: 7000000      //make sure this gas allocation isn't over 4M, which is the max
-        },
-        live: {
-            provider: function() {
-                return new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${projectId}`)
-            },
-            network_id: 1,
-            gas: 3000000,      //make sure this gas allocation isn't over 4M, which is the max
-            gasPrice: web3.utils.toWei('16', 'gwei')
+  // Uncommenting the defaults below
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  //
+  networks: {
+  development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+  },
+  ropsten: {
+    provider: function() {
+      return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`)
+    },
+    network_id: 3,
+    gas: 7000000      //make sure this gas allocation isn't over 4M, which is the max
+  },
+  kovan: {
+    provider: function() {
+      return new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${projectId}`)
+    },
+    network_id: 42,
+    gas: 7000000      //make sure this gas allocation isn't over 4M, which is the max
+  },
+  live: {
+    provider: function() {
+      return new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${projectId}`)
+    },
+    network_id: 1,
+    gas: 3000000,      //make sure this gas allocation isn't over 4M, which is the max
+    gasPrice: web3.utils.toWei('16', 'gwei')
 
         }
         //  test: {
